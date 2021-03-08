56cc37db6eec516f19b61c735cebcb0684a519cf
rayaneberrada <rayaneberrada44500@gmail.com>
2021-03-08 11:57:11 +0100

    Eureka server added to project

---
+++ EurekaServer/src/test/java/fr/openclassrooms/rayane/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.openclassrooms.rayane.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- TourGuide/src/main/java/tourGuide/Application.java
+++ TourGuide/src/main/java/tourGuide/Application.java
@@ -3,11 +3,12 @@ package tourGuide;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.util.Locale;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableFeignClients
 public class Application {
 
     public static void main(String[] args) {
